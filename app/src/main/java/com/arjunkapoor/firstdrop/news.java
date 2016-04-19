d01c4cceb3dc7aa4ff8c775587ff88492c7ed291
Arjun kapoor <arjunkapoor817@gmail.com>
2016-04-19 05:56:03 +0530

    news add

--- app/src/main/java/com/arjunkapoor/firstdrop/news.java
+++ app/src/main/java/com/arjunkapoor/firstdrop/news.java
@@ -1,19 +1,36 @@
 package com.arjunkapoor.firstdrop;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.InetAddress;
+import java.util.ArrayList;
+
+import it.gmariotti.cardslib.library.cards.actions.BaseSupplementalAction;
+import it.gmariotti.cardslib.library.cards.actions.TextSupplementalAction;
+import it.gmariotti.cardslib.library.cards.material.MaterialLargeImageCard;
+import it.gmariotti.cardslib.library.internal.Card;
 
 public class news extends AppCompatActivity {
     static boolean active = false;
+    LinearLayout LayoutOnNullCount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,13 +44,30 @@ public class news extends AppCompatActivity {
             Button newsButton = (Button) findViewById(R.id.new_buttonnews);
             newsButton.setTextColor(Color.parseColor("#ffffff"));
             newsButton.setBackgroundColor(Color.parseColor("#1f1a1a"));
-            RecyclerView recList = (RecyclerView) findViewById(R.id.reclylerview);
-            recList.setHasFixedSize(true);
-            LinearLayoutManager manager = new LinearLayoutManager(this);
-            manager.setOrientation(LinearLayoutManager.VERTICAL);
-            recList.setLayoutManager(manager);
-    }
 
+if(isNetworkAvailable() == false){
+    LinearLayout layoutexisting = (LinearLayout) findViewById(R.id.cardlinearlayoutnews);
+    layoutexisting.setVisibility(LinearLayout.GONE);
+    LayoutOnNullCount = new LinearLayout(this);
+    LayoutOnNullCount.setBackgroundColor(Color.parseColor("#F2F2F2"));
+    LayoutOnNullCount.setOrientation(LinearLayout.VERTICAL);
+    LinearLayout.LayoutParams LayoutOnNullCountParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+    LayoutOnNullCount.setLayoutParams(LayoutOnNullCountParams);
+    TextView Text = new TextView(this);
+    Text.setText("\n\n\n\n\n\n\n\n\nNo internet connection");
+    Text.setGravity(Gravity.CENTER);
+    Text.setTextSize(20);
+
+
+    LayoutOnNullCount.addView(Text);
+    LinearLayout layoutparent = (LinearLayout) findViewById(R.id.cardlinearlayoutparentnews);
+    layoutparent.addView(LayoutOnNullCount);
+Toast.makeText(this,"No connection detected",Toast.LENGTH_SHORT).show();
+
+
+
+}
+    }
 
 
 
@@ -68,5 +102,11 @@ public class news extends AppCompatActivity {
         intent.addCategory(Intent.CATEGORY_HOME);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
+    }
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
